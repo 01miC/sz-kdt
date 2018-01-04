356e95ebf145668a8b30ccb9f59c61c6c882ddf3
Michal Dokupil <michal@dokupil.pl>
2018-01-04 12:30:06 +0100

    Add main html with styles

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var imageSizeLeft = document.getElementById('js-imageResize-left');
+var imageSizeRight = document.getElementById('js-imageResize-right');
+
+imageSizeLeft.addEventListener("mouseover", function (e) {
+
+});
+
+imageSizeRight.addEventListener("mouseover", function (e) {
+    
+});
